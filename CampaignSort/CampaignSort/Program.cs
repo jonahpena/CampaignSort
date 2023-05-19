2b8f88e5f72953e2c3031a7af6e5a69a1b9d4e09
Jonah <jonahrpena@gmail.com>
2023-05-19 16:39:29 -0500

    added higher quality dummy data

--- CampaignSort/CampaignSort/Program.cs
+++ CampaignSort/CampaignSort/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using CampaignSort.ComparerClasses;
+﻿using CampaignSort.ComparerClasses;
 
 namespace CampaignSort
 {
@@ -8,132 +6,103 @@ namespace CampaignSort
     {
         static void Main(string[] args)
         {
-            var campaign1 = new Campaign
-            {
-                Name = "Campaign A",
-                StartDate = new DateTime(2023, 1, 1),
-                EndDate = new DateTime(2023, 12, 31),
-                TotalBudget = 5000.0,
-                NumberOfCustomersReached = 1000,
-                EngagementRate = 0.75,
-                ConversionRate = 0.10,
-                ClickThroughRate = 0.20,
-                ReturnOnInvestment = 1.5,
-                RevenueGenerated = 7500,
-                SocialMediaShares = 200,
-                CustomerSatisfaction = 0.90
+            // Initialize your campaigns
+            List<Campaign> campaigns = new List<Campaign>{
+            new Campaign
+    {
+        Name = "Hoosier Holiday Hustle",
+        StartDate = new DateTime(2023, 11, 1),
+        EndDate = new DateTime(2023, 12, 25),
+        TotalBudget = 15000.0,
+        NumberOfCustomersReached = 50000,
+        EngagementRate = 0.65,
+        ConversionRate = 0.15,
+        ClickThroughRate = 0.07,
+        ReturnOnInvestment = 1.3,
+        RevenueGenerated = 19500.0,
+        SocialMediaShares = 1200,
+        CustomerSatisfaction = 0.85
+    },
+    new Campaign
+    {
+        Name = "Indy 500 Special",
+        StartDate = new DateTime(2023, 5, 1),
+        EndDate = new DateTime(2023, 5, 31),
+        TotalBudget = 10000.0,
+        NumberOfCustomersReached = 40000,
+        EngagementRate = 0.70,
+        ConversionRate = 0.10,
+        ClickThroughRate = 0.05,
+        ReturnOnInvestment = 1.2,
+        RevenueGenerated = 12000.0,
+        SocialMediaShares = 800,
+        CustomerSatisfaction = 0.80
+    },
+    new Campaign
+    {
+        Name = "Spring Fever Frenzy",
+        StartDate = new DateTime(2023, 4, 1),
+        EndDate = new DateTime(2023, 4, 30),
+        TotalBudget = 8000.0,
+        NumberOfCustomersReached = 30000,
+        EngagementRate = 0.75,
+        ConversionRate = 0.20,
+        ClickThroughRate = 0.10,
+        ReturnOnInvestment = 1.5,
+        RevenueGenerated = 12000.0,
+        SocialMediaShares = 900,
+        CustomerSatisfaction = 0.90
+    },
+    new Campaign
+    {
+        Name = "Autumn Adventures",
+        StartDate = new DateTime(2023, 9, 1),
+        EndDate = new DateTime(2023, 11, 30),
+        TotalBudget = 12000.0,
+        NumberOfCustomersReached = 45000,
+        EngagementRate = 0.60,
+        ConversionRate = 0.12,
+        ClickThroughRate = 0.06,
+        ReturnOnInvestment = 1.4,
+        RevenueGenerated = 16800.0,
+        SocialMediaShares = 1000,
+        CustomerSatisfaction = 0.88
+    },
+    new Campaign
+    {
+        Name = "Summer Sizzler Sale",
+        StartDate = new DateTime(2023, 6, 1),
+        EndDate = new DateTime(2023, 8, 31),
+        TotalBudget = 20000.0,
+        NumberOfCustomersReached = 60000,
+        EngagementRate = 0.68,
+        ConversionRate = 0.18,
+        ClickThroughRate = 0.08,
+        ReturnOnInvestment = 1.6,
+        RevenueGenerated = 32000.0,
+        SocialMediaShares = 1500,
+        CustomerSatisfaction = 0.92
+    },
+                new Campaign
+                {
+                    Name = "Emarsys Valentines Special",
+                    // ... other attributes here
+                }
             };
 
-            var campaign2 = new Campaign
-            {
-                Name = "Campaign B",
-                StartDate = new DateTime(2023, 2, 1),
-                EndDate = new DateTime(2023, 11, 30),
-                TotalBudget = 7000.0,
-                NumberOfCustomersReached = 2000,
-                EngagementRate = 0.85,
-                ConversionRate = 0.15,
-                ClickThroughRate = 0.25,
-                ReturnOnInvestment = 2.0,
-                RevenueGenerated = 14000,
-                SocialMediaShares = 300,
-                CustomerSatisfaction = 0.95
-            };
+            CampaignComparerFactory factory = new CampaignComparerFactory();
 
-            // add more campaigns as needed
-
-            var campaigns = new List<Campaign> { campaign1, campaign2 };
+            // Change the string parameter to the attribute you wish to sort by
+            ICampaignComparer comparer = factory.GetCampaignComparer("Name");
 
             var sorter = new CampaignSorter();
+            var sortedCampaigns = sorter.Sort(campaigns, comparer);
 
-            // Sorting by Name
-            Console.WriteLine("Sorting by Name:");
-            var sortedByName = sorter.Sort(campaigns, new NameComparer());
-            foreach (var campaign in sortedByName)
+            // Instead of just printing the first two, loop over and print all campaigns
+            foreach (var campaign in sortedCampaigns)
             {
                 Console.WriteLine(campaign.Name);
             }
-
-            // Sorting by StartDate
-            Console.WriteLine("\nSorting by StartDate:");
-            var sortedByStartDate = sorter.Sort(campaigns, new StartDateComparer());
-            foreach (var campaign in sortedByStartDate)
-            {
-                Console.WriteLine($"{campaign.Name}, Start Date: {campaign.StartDate}");
-            }
-
-            // Repeat this for other comparers (EndDate, TotalBudget, NumberOfCustomersReached, etc.)
-            
-            Console.WriteLine("\nSorting by EndDate:");
-            var sortedByEndDate = sorter.Sort(campaigns, new EndDateComparer());
-            foreach (var campaign in sortedByEndDate)
-            {
-                Console.WriteLine($"{campaign.Name}, End Date: {campaign.EndDate}");
-            }
-            
-            Console.WriteLine("\nSorting by TotalBudget:");
-            var sortedByTotalBudget = sorter.Sort(campaigns, new TotalBudgetComparer());
-            foreach (var campaign in sortedByTotalBudget)
-            {
-                Console.WriteLine($"{campaign.Name}, Total Budget: {campaign.TotalBudget}");
-            }
-            
-            Console.WriteLine("\nSorting by NumberOfCustomersReached:");
-            var sortedByNumberOfCustomersReached = sorter.Sort(campaigns, new NumberOfCustomersReachedComparer());
-            foreach (var campaign in sortedByNumberOfCustomersReached)
-            {
-                Console.WriteLine($"{campaign.Name}, Number Of Customers Reached: {campaign.NumberOfCustomersReached}");
-            }
-            
-            Console.WriteLine("\nSorting by EngagementRate:");
-            var sortedByEngagementRate = sorter.Sort(campaigns, new EngagementRateComparer());
-            foreach (var campaign in sortedByEngagementRate)
-            {
-                Console.WriteLine($"{campaign.Name}, Engagement Rate: {campaign.EngagementRate}");
-            }
-            
-            Console.WriteLine("\nSorting by ConversionRate:");
-            var sortedByConversionRate = sorter.Sort(campaigns, new ConversionRateComparer());
-            foreach (var campaign in sortedByConversionRate)
-            {
-                Console.WriteLine($"{campaign.Name}, Conversion Rate: {campaign.ConversionRate}");
-            }
-            
-            Console.WriteLine("\nSorting by ClickThroughRate:");
-            var sortedByClickThroughRate = sorter.Sort(campaigns, new ClickThroughRateComparer());
-            foreach (var campaign in sortedByClickThroughRate)
-            {
-                Console.WriteLine($"{campaign.Name}, Click Through Rate: {campaign.ClickThroughRate}");
-            }
-            
-            Console.WriteLine("\nSorting by ReturnOnInvestment:");
-            var sortedByReturnOnInvestment = sorter.Sort(campaigns, new ReturnOnInvestmentComparer());
-            foreach (var campaign in sortedByReturnOnInvestment)
-            {
-                Console.WriteLine($"{campaign.Name}, Return On Investment: {campaign.ReturnOnInvestment}");
-            }
-            
-            Console.WriteLine("\nSorting by RevenueGenerated:");
-            var sortedByRevenueGenerated = sorter.Sort(campaigns, new RevenueGeneratedComparer());
-            foreach (var campaign in sortedByRevenueGenerated)
-            {
-                Console.WriteLine($"{campaign.Name}, Revenue Generated: {campaign.RevenueGenerated}");
-            }
-            
-            Console.WriteLine("\nSorting by SocialMediaShares:");
-            var sortedBySocialMediaShares = sorter.Sort(campaigns, new SocialMediaSharesComparer());
-            foreach (var campaign in sortedBySocialMediaShares)
-            {
-                Console.WriteLine($"{campaign.Name}, Social Media Shares: {campaign.SocialMediaShares}");
-            }
-            
-            Console.WriteLine("\nSorting by CustomerSatisfaction:");
-            var sortedByCustomerSatisfaction = sorter.Sort(campaigns, new CustomerSatisfactionComparer());
-            foreach (var campaign in sortedByCustomerSatisfaction)
-            {
-                Console.WriteLine($"{campaign.Name}, Customer Satisfaction: {campaign.CustomerSatisfaction}");
-            }
-            
         }
     }
 }
