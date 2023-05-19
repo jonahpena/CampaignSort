0afb7aea7a20bc20e889104c9f022485744672f5
Jonah <jonahrpena@gmail.com>
2023-05-19 16:44:54 -0500

    added additonal attribute handling to GetCampaignComparer

--- CampaignSort/CampaignSort/CompaignComparerFactory.cs
+++ CampaignSort/CampaignSort/CompaignComparerFactory.cs
@@ -6,12 +6,35 @@ public class CampaignComparerFactory
 {
     public ICampaignComparer GetCampaignComparer(string attribute)
     {
-        switch (attribute.ToLower())
+        switch (attribute)
         {
-            case "name":
+            case "Name":
                 return new NameComparer();
+            case "StartDate":
+                return new StartDateComparer();
+            case "EndDate":
+                return new EndDateComparer();
+            case "TotalBudget":
+                return new TotalBudgetComparer();
+            case "NumberOfCustomersReached":
+                return new NumberOfCustomersReachedComparer();
+            case "EngagementRate":
+                return new EngagementRateComparer();
+            case "ConversionRate":
+                return new ConversionRateComparer();
+            case "ClickThroughRate":
+                return new ClickThroughRateComparer();
+            case "ReturnOnInvestment":
+                return new ReturnOnInvestmentComparer();
+            case "RevenueGenerated":
+                return new RevenueGeneratedComparer();
+            case "SocialMediaShares":
+                return new SocialMediaSharesComparer();
+            case "CustomerSatisfaction":
+                return new CustomerSatisfactionComparer();
             default:
                 throw new ArgumentException($"Invalid attribute: {attribute}");
         }
     }
+
 }
